84faab6e9f2854da625c6d72426e50374dabf1b8
Konstantin <kons1979@mail.ru>
2022-09-01 08:21:00 +0400

    task62 finished

---
+++ task62/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 62. Заполните спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 1 2 3 4
+// 12 13 14 5
+// 11 16 15 6
+// 10 9 8 7
+Console.Write("Введите кол-во строк : ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов : ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] OurMatrix = new int[m,n];
+
+void PrintMatrix(int[,] matrix){
+for (int i=0; i<matrix.GetLength(0); i++){
+    for (int j=0; j<matrix.GetLength(1); j++){Console.Write(matrix[i,j]+" ");}
+    Console.WriteLine();}}
+
+int y=0;
+int x=0;
+int Number = 1;
+OurMatrix[y,x] = Number;
+int check = 1;
+
+while (check==1) {
+    check = 0;
+    while (x<OurMatrix.GetLength(1)-1) {
+        if (OurMatrix[y,x+1]==0) {Number++;check = 1;OurMatrix[y,x+1] = Number;x++;} 
+        else {break;}}
+    while (y<OurMatrix.GetLength(0)-1) {
+        if (OurMatrix[y+1,x]==0) {Number++;check = 1;OurMatrix[y+1,x] = Number;y++;} 
+        else {break;}}    
+    while (x>0) {
+        if (OurMatrix[y,x-1]==0) {Number++;check = 1;OurMatrix[y,x-1] = Number;x--;} 
+        else {break;}}
+    while (y>0) {
+        if (OurMatrix[y-1,x]==0) {Number++;check = 1;OurMatrix[y-1,x] = Number;y--;} 
+        else {break;}}}
+
+PrintMatrix(OurMatrix);
+
+
+
